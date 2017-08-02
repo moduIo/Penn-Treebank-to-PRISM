ab1b4f3140493c4cde2eadf14c80b50129fbbd89
Tim <ethosscream@yahoo.com>
2017-08-02 12:59:58 -0400

    Add formatting sections for model and declaration parts of PRISM program

--- translator.py
+++ translator.py
@@ -16,3 +16,31 @@ header += '\n% Source: http://rjida.meijo-u.ac.jp/prism/download/prism22.pdf'
 header += '\n%%%'
 
 print(header)
+
+#
+# Format Declarations section of program
+#
+declarations = '\n%%------------------------------------'
+declarations += '\n%%  Declarations:'
+declarations += '\n%%------------------------------------'
+declarations += '\n\n%'
+declarations += '\n% Probabilistic production rules'
+declarations += '\n%'
+
+print(declarations)
+
+#
+# Format Modeling section of program
+#
+model = '\n%%------------------------------------'
+model += '\n%%  Modeling part:'
+model += '\n%%------------------------------------'
+model += '\n\npcfg(L) :- pcfg(\'S\', L-[]).'
+model += '\n\npcfg(LHS,L0-L1) :-'
+model += '\n    ( nonterminal(LHS) -> msw(LHS, RHS), proj(RHS, L0-L1)'
+model += '\n    ; L0 = [LHS|L1]'
+model += '\n    ).'
+model += '\n\nproj([], L-L).'
+model += '\nproj([X|Xs], L0-L1) :- pcfg(X, L0-L2), proj(Xs, L2-L1).'
+
+print(model)
